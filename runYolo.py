a62adf5ddcaf48fe79d9519f566bf0503f9dc89b
u3578231 <98687796+u3578231@users.noreply.github.com>
2024-01-08 20:29:48 +0800

    Update runYolo.py

--- runYolo.py
+++ runYolo.py
@@ -1,37 +1,60 @@
-#README
-# Run $ pip install ultralytics 
-# If there's AttributeError and some libraries are missing, such as cv2, cpuinfo, ultralytics, you're missing some libraries then just 
-# search the libaries and pip install them back.
-# Simple python3 runYolo.py, then it will display your webcam with detection boxes, terminal will show each detected box name and bounding box coordinates.
-# for more information in working with the result detection, follow documentations in https://docs.ultralytics.com/reference/engine/results/#ultralytics.engine.results.Results.verbose
 from ultralytics import YOLO
 import cv2
-from ultralytics.utils.plotting import Annotator 
+from ultralytics.utils.plotting import Annotator
+import time
 
 model = YOLO('yolov8n.pt')
-cap = cv2.VideoCapture(0)
-cap.set(3, 640)
-cap.set(4, 480)
+print("model =", model)
 
+cap = cv2.VideoCapture(0)
+if not cap.isOpened():
+    print("Error opening video capture.")
+    exit()
+
+prev_b = None  # Variable to store previous frame's boundary box coordinates
+degree_constant = 0.036
 while True:
     _, img = cap.read()
-    
-    results = model.predict(img)
 
-    for r in results:
+    if img.shape[0] > 0 and img.shape[1] > 0:
+        results = model.predict(img)
         
-        annotator = Annotator(img)
-        
-        boxes = r.boxes
-        for box in boxes:
+        for r in results:
+            annotator = Annotator(img)
+            boxes = r.boxes
             
-            b = box.xyxy[0]  # get box coordinates in (left, top, right, bottom) format
-            c = box.cls # get box class value, use model.names[int(c)] to get name of the class, such as person, remote
-            annotator.box_label(b, model.names[int(c)]) # display the bounding box on screen 
-            print(model.names[int(c)], ":", b) # Print the box coordinate 
-          
-    img = annotator.result()  
-    cv2.imshow('YOLO V8 Detection', img)     
+            for box in boxes:
+                b = box.xyxy[0]  # get box coordinates in (left, top, right, bottom) format
+                c = box.cls  # get box class value
+                class_name = model.names[int(c)]
+                if class_name == 'person':
+                    if prev_b is not None:
+                        prev_left, prev_left_top, prev_right, prev_right_bottom= prev_b
+                        curr_left, curr_left_top, curr_right, curr_right_bottom = b
+
+                        # Calculate the offset between previous and current frame boundary box
+                        left_offset = curr_left - prev_left
+                        right_offset = curr_right - prev_right
+                        left_top_offset = curr_left_top - prev_left_top
+                        right_bottom_offset = curr_right_bottom - prev_right_bottom
+                        # Determine the movement based on the offset
+                        if left_offset > 17:
+                            print("Target shifted left")
+                        elif left_offset < -17:
+                            print("Target shifted right")
+                        else:
+                            print("Target did not move horizontally")
+                        print("degree shifted =",left_offset*degree_constant)
+                    annotator.box_label(b, class_name)  # display the bounding box on screen
+                    print(class_name, ":", b)  # Print the box coordinate
+
+                    prev_b = b  # Store current frame's boundary box coordinates as previous
+
+        img = annotator.result()
+        cv2.imshow('YOLO V8 Detection', img)
+
+    time.sleep(1)
+
     if cv2.waitKey(1) & 0xFF == ord(' '):
         break
 
